41d5c750f944f8cbc0a433eb1323513c1dc27560
Jan Schönberger <janschoenberger@posteo.de>
2020-11-26 10:42:54 +0100

    Model.cs

---
+++ WebApplication1/Models/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public  string Firstname{ get; set;}
+        public string LastName { get; set; }
+
+        public string Nickname { get; set; }
+
+        public int Place { get; set; }
+
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+
+        public bool IsDeleted { get; set; } = false ;
+
+
+
+    }
+}
